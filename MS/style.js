40292f0bb480b129bdf7a9b7ea4bc671de3202b2
AsifHasanChowdhury <72686397+AsifHasanChowdhury@users.noreply.github.com>
2021-06-04 01:53:37 +0600

    Update style.js

--- MS/style.js
+++ MS/style.js
@@ -73,3 +73,26 @@ internalmodalBg2.classList.remove('bgs-active2','bg-active2');
 
       
 });
+
+
+/*owl carousel */
+$('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      dots:false,
+      autoplay:true,
+      autoplayTimeout:4000,
+      autoHeight:false,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:1
+          }
+      }
+      })
